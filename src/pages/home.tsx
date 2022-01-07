89a09700a805596b2c5bd2652653e1248c6d0ea1
Sky-seed0215 <k.nao02.ki15@gmail.com>
2022-01-07 10:58:42 +0900

    スクロールアップボタンの作成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '../components/layouts/Header';
+import ScrollUp from '../components/utils/ScrollUp';
 
 const Home: React.FC = () => {
     return (
         <>
         <Header />
         <h1>Hello World!!</h1>
+        <ScrollUp />
         </>
     )
 }
